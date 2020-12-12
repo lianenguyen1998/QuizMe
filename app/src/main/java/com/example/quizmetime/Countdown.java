cdeaa831ab77910f2872fdffd84665a228d72c2e
majamichaelis <maja.michaelis@study.hs-duesseldorf.de>
2020-12-12 21:45:47 +0100

    countdown beim Clickenn

---
+++ app/src/main/java/com/example/quizmetime/Countdown.java
@@ -0,0 +1,9 @@
+package com.example.quizmetime;
+
+public interface Countdown {
+
+    static final long COUNTDOWN_IN_MILLIS = 30000;
+
+    void startCountdown();
+    void updateCountdownText();
+}
