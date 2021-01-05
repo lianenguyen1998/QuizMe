244db616a9b9af7a43647c4b299994c493c9c3d1
anonym98 <axisooriyakanthan@gmail.com>
2021-01-05 17:17:50 +0100

    Layout Stand: 05.01.2021

--- app/src/main/java/com/example/quizme_layout/Highscoreliste.java
+++ app/src/main/java/com/example/quizme_layout/Highscoreliste.java
@@ -2,13 +2,55 @@ package com.example.quizme_layout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Highscoreliste extends AppCompatActivity {
+
+    // ---- StartseiteButton
+    private Button zurückzurStartseite;
+
+    // ---- WeiterSpielenButton
+    private Button weiterSpielen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.highscoreliste);
+
+        zurückzurStartseite = (Button) findViewById(R.id.zurückzurStartseite);
+        weiterSpielen = (Button) findViewById(R.id.weiterSpielen);
+
+        zurückzurStartseite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                openzurückzurStartseite();
+            }
+        });
+
+        weiterSpielen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                openweiterSpielen();
+            }
+        });
     }
+
+    // ---- bringt uns zurück Startseite
+    public void openzurückzurStartseite() {
+        Intent intent = new Intent(this, Startseite.class);
+        startActivity(intent);
+    }
+
+    // ---- bringt uns zurück zum Quiz
+    public void openweiterSpielen() {
+        Intent intent = new Intent(this, Quiz.class);
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/com/example/quizme_layout/Startseite.java
+++ app/src/main/java/com/example/quizme_layout/Startseite.java
@@ -1,8 +1,10 @@
 package com.example.quizme_layout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
